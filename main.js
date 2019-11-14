1a823207ea05e996e008b3de728d9a7230a683cd
Waren Long <waren@sourced.tech>
2019-11-14 18:05:22 +0100

    Add About page
    Signed-off-by: Waren Long <waren@sourced.tech>

--- main.js
+++ main.js
@@ -5,24 +5,19 @@ var TxtType = function(el, toRotate, period) {
     this.period = parseInt(period, 10) || 2000;
     this.txt = '';
     this.tick();
-    this.isDeleting = false;
 };
 
 TxtType.prototype.tick = function() {
     var i = this.loopNum % this.toRotate.length;
     var fullTxt = this.toRotate[i];
-
     this.txt = fullTxt.substring(0, this.txt.length + 1);
     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
     var that = this;
     var delta = 200 - Math.random() * 100;
-
     if (this.txt.length === 16) {
     delta = this.period;
     this.loopNum++;
     }
-
     setTimeout(function() {
     that.tick();
     }, delta);
